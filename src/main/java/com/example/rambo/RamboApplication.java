920def2ed0e548be143b562143599e9e9c327279
Githubrambo@123 <20a91a0579@aec.edu.in>
2023-11-13 21:53:27 +0530

    first

---
+++ src/main/java/com/example/rambo/RamboApplication.java
@@ -0,0 +1,13 @@
+package com.example.rambo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamboApplication.class, args);
+	}
+
+}
